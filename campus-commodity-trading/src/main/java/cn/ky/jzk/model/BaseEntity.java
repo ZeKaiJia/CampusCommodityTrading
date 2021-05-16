98087938f739ce367f6aeb1970b5c01fd4607f7e
ZeKai Jia <jiazekai1003@gmail.com>
2021-05-16 11:42:24 +0800

    [feature] 添加了用户权限表和相关实体，准备整合Shiro [feature] 抽象基本实体类，准备让所有实体继承，将大幅修改数据库表内容。

---
+++ campus-commodity-trading/src/main/java/cn/ky/jzk/model/BaseEntity.java
@@ -0,0 +1,54 @@
+package cn.ky.jzk.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 基本实体类
+ * The type Abstract entity.
+ * BaseEntity
+ * @Author: Kevin
+ * @Date: 2020 /6/2 3:42 下午
+ */
+@Data
+@ApiModel(value = "基本实体类", description = "基本实体类")
+public abstract class BaseEntity implements Serializable {
+    /**
+     * 序列化
+     * The constant serialVersionUID.
+     */
+    protected static final long serialVersionUID = 4846381295033205916L;
+    /**
+     * UTC创建时间
+     * The Utc create.
+     */
+    @ApiModelProperty(value = "创建时间", required = true)
+    protected long utcCreate;
+     /**
+      * UTC修改时间
+     * The Utc modify.
+     */
+    @ApiModelProperty(value = "修改时间", required = true)
+    protected long utcModify;
+    /**
+     * 修改人
+     * The Modify by.
+     */
+    @ApiModelProperty(value = "修改人", required = true)
+    protected String modifyBy;
+    /**
+     * 是否有效
+     * The Valid.
+     */
+    @ApiModelProperty(value = "是否有效", required = true)
+    protected Boolean valid;
+    /**
+     * 备注
+     * The Remark.
+     */
+    @ApiModelProperty(value = "备注", required = false)
+    protected String remark;
+}
---
+++ campus-commodity-trading/src/main/java/cn/ky/jzk/model/Permission.java
@@ -0,0 +1,38 @@
+package cn.ky.jzk.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 权限类
+ * Role
+ * 权限实体类
+ * @author kevin
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "权限实体类", description = "权限实体类")
+public class Permission {
+    /**
+     * 序号
+     * The Permission ID.
+     */
+    @ApiModelProperty(value = "序号", example = "1", required = true)
+    private String perId;
+    /**
+     * 权限名
+     * The Permission Name.
+     */
+    @ApiModelProperty(value = "权限名", example = "添加用户", required = true)
+    private String perName;
+    /**
+     * 权限代码
+     * The Permission Code.
+     */
+    @ApiModelProperty(value = "权限代码", example = "user:select", required = true)
+    private String perCode;
+}
